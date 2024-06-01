883da5182339d53f4258051f4d838f9c9c2014fc
Mathys <75971034+opelooo@users.noreply.github.com>
2024-06-01 14:28:54 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -41,7 +41,7 @@ async def predict(url: str, model_name: str):
     if 'tokenizer' not in globals():
         global tokenizer
         tokenizer = Tokenizer()
-        tokenizer.word_index = read_tokens()
+        tokenizer.fit_on_tekts(read_tokens())
         tokenizer.fit_on_texts(cleaned_tokens)
 
     sequences = tokenizer.texts_to_sequences(cleaned_tokens)
@@ -49,7 +49,6 @@ async def predict(url: str, model_name: str):
     sys.stdout.flush()
 
     X_padded = pad_sequences([sequences], maxlen=max_sequence_length)
-    print("X_padded: ", X_padded)
     print("X_padded dtype: ", X_padded.dtype)
     sys.stdout.flush()
     del tokenizer
